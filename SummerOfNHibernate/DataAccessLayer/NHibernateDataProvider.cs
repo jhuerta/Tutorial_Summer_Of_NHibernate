455b92dfa9ea4d20566ccc5244c37bbcd666366e
Juan Huerta <juan.huerta@gmail.com>
2012-05-14 21:36:36 +0800

    Lesson 2 - Up to criteria API

--- SummerOfNHibernate/DataAccessLayer/NHibernateDataProvider.cs
+++ SummerOfNHibernate/DataAccessLayer/NHibernateDataProvider.cs
@@ -1,36 +1,91 @@
-﻿using DataTransfer;
+﻿using System;
+using System.Collections.Generic;
+using DataTransfer;
+using NHibernate;
 using NHibernate.Cfg;
 
 namespace DataAccessLayer
 {
     public class NHibernateDataProvider
     {
+
         public Customer GetCustomerById(int customerId)
         {
-            // 1. Get config file
-            // 2. Factory will build session with the config file
-            // 3. Both factory and config file are disposable
-            // Therefore: We can combine and not stored variables
-            //var config = new Configuration();
-            //config.Configure();
-            //var sessionFactory = config.BuildSessionFactory();
-            //var session = sessionFactory.OpenSession();
+            var session = GetSession();
 
+            return session.Get<Customer>(customerId);
+        }
+
+        private static ISession GetSession()
+        {
             var configuration = new Configuration();
+
             configuration.Configure("hibernate.cfg.xml");
+
             var sessionFactory = configuration.BuildSessionFactory();
 
             var session = sessionFactory.OpenSession();
-
-            // This would work but the risk is that it will try to cast the return into an object
-            // This cast operation may fail.
-            //return (DataTransfer.Customer) session.Get(typeof (DataTransfer.Customer), customerId);
-
-            // Because the cast may fail, we'd rather go for  generics.
-            return session.Get<Customer>(customerId);
-
+            return session;
         }
 
+        /*public Customer GetCustomerById(int customerId)
+        {
+             1. Get config file
+             2. Factory will build session with the config file
+             3. Both factory and config file are disposable
+             Therefore: We can combine and not stored variables
+            var config = new Configuration();
+            config.Configure();
+            var sessionFactory = config.BuildSessionFactory();
+            var session = sessionFactory.OpenSession();
+
+
+             This would work but the risk is that it will try to cast the return into an object
+             This cast operation may fail.
+            return (DataTransfer.Customer) session.Get(typeof (DataTransfer.Customer), customerId);
+
+             Because the cast may fail, we'd rather go for  generics.
+             return session.Get<Customer>(customerId);
+        }*/
+
+        public IList<Customer> GetCustomersByFirstname(string firstname)
+        {
+            var session = GetSession();
+
+            var queryString = string.Format("select c from Customer c where c.Firstname = '{0}'", firstname);
+
+            return session.CreateQuery(queryString).List<Customer>();
+
+        }
+        
+        public IList<Customer> GetCustomerByFirstnameWithParameters(string firstname)
+        {
+            var session = GetSession();
+
+            const string queryString = "select c from Customer c where c.Firstname=:name";
+
+            return session.CreateQuery(queryString).SetString("name",firstname).List<Customer>();
+        }
+
+        public IList<Customer> GetCustomerByFirstnameAndLastname(string firstname, string lastname)
+        {
+            var session = GetSession();
+
+            const string queryString = "select c from Customer c where c.Firstname=:fName and c.Lastname=:lName";
+
+            return session.CreateQuery(queryString)
+                .SetString("fName", firstname)
+                .SetString("lName", lastname)
+                .List<Customer>();
+        }
+
+        public IList<Customer> GetCustomersWithIdGreaterThan(int id)
+        {
+            ISession session = GetSession();
+
+            const string queryString = "select c from Customer c where c.Id > :id";
+
+            return session.CreateQuery(queryString).SetInt32("id", id).List<Customer>();
+        }
     }
 }
-
--- SummerOfNHibernate/DataAccessLayerTest/DataAccessLayerTest.cs
+++ SummerOfNHibernate/DataAccessLayerTest/DataAccessLayerTest.cs
@@ -7,21 +7,99 @@ namespace DataAccessLayerTest
     [TestFixture]
     public class DataAccessLayerTests
     {
+        private NHibernateDataProvider provider;
+        const string customerFirstname = "Juan";
+        const string customerLastname = "Huerta";
+        const int numberOfOcurances = 1;
 
-
+        [FixtureSetUp]
+        public void FixtureSetup()
+        {
+            provider = new NHibernateDataProvider();
+        }
 
         [Test]
         public void CanGetCustomerId()
         {
-            // Comment
+            var customerId = 1;
 
-            var provider = new NHibernateDataProvider();
-            var actual = provider.GetCustomerById(1).Id;
-            Assert.AreEqual(1, actual);
+            var actual = provider.GetCustomerById(customerId);
 
-            // Comment
+            Assert.AreEqual(customerId, actual.Id);
+   
+        }
+
+        [Test]
+        public void ReturnsNullIfItdoesNotFindACustomer()
+        {
+            const int customerId = -1;
+
+            var actual = provider.GetCustomerById(customerId);
+
+            Assert.AreEqual(null, actual);
 
         }
-    }
 
+
+        [Test]
+        public void CanGetCustomersByFirsname()
+        {
+
+            var customers = provider.GetCustomersByFirstname("Juan");
+
+            foreach (var customer in customers)
+            {
+                Assert.AreEqual(customer.Firstname, customerFirstname);
+            }
+
+            Assert.AreEqual(customers.Count, numberOfOcurances);
+        }
+
+        [Test]
+        public void CanGetCustomersByFirsnameWithParameters()
+        {
+
+            var customers = provider.GetCustomerByFirstnameWithParameters(customerFirstname);
+
+            foreach (var customer in customers)
+            {
+                Assert.AreEqual(customer.Firstname, customerFirstname);
+            }
+
+            Assert.AreEqual(customers.Count, numberOfOcurances);
+
+        }
+
+        [Test]
+        public void CanGetCustomersByFirsnameAndLastName()
+        {
+
+            var customers = provider.GetCustomerByFirstnameAndLastname(customerFirstname,customerLastname);
+
+            foreach (var customer in customers)
+            {
+                Assert.AreEqual(customer.Firstname, customerFirstname);
+                Assert.AreEqual(customer.Lastname, customerLastname);
+            }
+            
+            Assert.AreEqual(customers.Count, numberOfOcurances);
+        }
+
+        [Test]
+        public void CanGetCustomersGreaterThanId()
+        {
+            var id = 5;
+            const int minimumNumerOfOccurances = 2;
+            var customers = provider.GetCustomersWithIdGreaterThan(id);
+
+            foreach (var customer in customers)
+            {
+                Assert.GreaterThan(customer.Id,id);
+            }
+
+            
+            Assert.GreaterThan(customers.Count, minimumNumerOfOccurances);
+        }
+
+    }
 }
