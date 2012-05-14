0c6cb1ea216fc897c5c9bc3c2e80cfab6482bd3c
Juan Huerta <juan.huerta@gmail.com>
2012-05-14 15:06:47 +0800

    First Commit From Office

--- SummerOfNHibernate/DataAccessLayerTest/DataAccessLayerTest.cs
+++ SummerOfNHibernate/DataAccessLayerTest/DataAccessLayerTest.cs
@@ -13,6 +13,7 @@ namespace DataAccessLayerTest
         [Test]
         public void CanGetCustomerId()
         {
+            // Comment
             var provider = new NHibernateDataProvider();
             var actual = provider.GetCustomerById(1).Id;
             Assert.AreEqual(1, actual);
