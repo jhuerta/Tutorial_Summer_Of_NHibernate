4fb9b556f63acd27c2ef2e87a565bff17882e71d
U-BLACK_CUCUMBER\Juan Huerta <Juan Huerta@black_cucumber.(none)>
2012-05-16 07:41:38 +0800

    End of Chapter 2a - Adding the 2 new files for data mapping customized objects from ddbb

---
+++ SummerOfNHibernate/DataTransfer/CustomerFirstnameCounter.cs
@@ -0,0 +1,26 @@
+﻿namespace DataTransfer
+{
+    public class CustomerFirstnameCounter
+    {
+        private string _firstname;
+        private long _count;
+
+        public CustomerFirstnameCounter(string firstname, long count)
+        {
+            _firstname = firstname;
+            _count = count;
+        }
+
+        public string Firstname
+        {
+            get { return _firstname; }
+            set { _firstname = value; }
+        }
+
+        public long Count
+        { 
+            get { return _count; }
+            set { _count = value; }
+        }
+    }
+}
